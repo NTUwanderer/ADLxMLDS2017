908dccd5d0a2d7344ff23f1a12ec712ec7e4d41e
NTUwanderer <harveyycyang@gmail.com>
2017-11-09 19:41:28 +0800

    Remove redundant lines in model.py

--- hw2/model.py
+++ hw2/model.py
@@ -187,7 +187,7 @@ learning_rate = 0.001
 CURSOR_UP_ONE = '\033[F'
 ERASE_LINE = '\033[K'
 
-def get_video_train_data(video_label_path):
+def get_video_data(video_label_path):
     with open(video_label_path, 'r') as f:
         captions = json.load(f)
     return captions
@@ -233,7 +233,7 @@ def preProBuildWordVocab(sentence_iterator, word_count_threshold=5):
     return wordtoix, ixtoword, bias_init_vector
 
 def train():
-    train_captions = get_video_train_data(video_train_label_path)
+    train_captions = get_video_data(video_train_label_path)
 
     captions_list = list([])
     v_ids = list([])
@@ -378,60 +378,6 @@ def train():
             saver.save(sess, os.path.join(model_path, 'model'), global_step=epoch)
 
     loss_fd.close()
-
-def test(model_path='./models/model-100'):
-    test_data = get_video_test_data(video_test_data_path, video_test_feat_path)
-    test_videos = test_data['video_path'].unique()
-
-    ixtoword = pd.Series(np.load('./data/ixtoword.npy').tolist())
-
-    bias_init_vector = np.load('./data/bias_init_vector.npy')
-
-    model = Video_Caption_Generator(
-            dim_image=dim_image,
-            n_words=len(ixtoword),
-            dim_hidden=dim_hidden,
-            batch_size=batch_size,
-            n_lstm_steps=n_frame_step,
-            n_video_lstm_step=n_video_lstm_step,
-            n_caption_lstm_step=n_caption_lstm_step,
-            bias_init_vector=bias_init_vector)
-
-    video_tf, video_mask_tf, caption_tf, probs_tf, last_embed_tf = model.build_generator()
-
-    sess = tf.InteractiveSession()
-
-    saver = tf.train.Saver()
-    saver.restore(sess, model_path)
-
-    test_output_txt_fd = open('S2VT_results.txt', 'w')
-    for idx, video_feat_path in enumerate(test_videos):
-        print(idx, video_feat_path)
-
-        video_feat = np.load(video_feat_path)[None,...]
-        #video_feat = np.load(video_feat_path)
-        #video_mask = np.ones((video_feat.shape[0], video_feat.shape[1]))
-        if video_feat.shape[1] == n_frame_step:
-            video_mask = np.ones((video_feat.shape[0], video_feat.shape[1]))
-        else:
-            continue
-            #shape_templete = np.zeros(shape=(1, n_frame_step, 4096), dtype=float )
-            #shape_templete[:video_feat.shape[0], :video_feat.shape[1], :video_feat.shape[2]] = video_feat
-            #video_feat = shape_templete
-            #video_mask = np.ones((video_feat.shape[0], n_frame_step))
-
-        generated_word_index = sess.run(caption_tf, feed_dict={video_tf:video_feat, video_mask_tf:video_mask})
-        generated_words = ixtoword[generated_word_index]
-
-        punctuation = np.argmax(np.array(generated_words) == '<eos>') + 1
-        generated_words = generated_words[:punctuation]
-
-        generated_sentence = ' '.join(generated_words)
-        generated_sentence = generated_sentence.replace('<bos> ', '')
-        generated_sentence = generated_sentence.replace(' <eos>', '')
-        print(generated_sentence,'\n')
-        test_output_txt_fd.write(video_feat_path + '\n')
-        test_output_txt_fd.write(generated_sentence + '\n\n')
 
 def main():
     train()
