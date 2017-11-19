3904dd26d3bb6c203bbcd5fae8fb98535dc70e52
NTUwanderer <harveyycyang@gmail.com>
2017-11-19 21:47:03 +0800

    hw2 report.pdf & model_seq2seq.py

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -1,3 +1,2 @@
-python model.py $1 -t -m final_model/final -o $2
-python model.py $1 -t -p -m final_model/final -o $3
-
+python model_seq2seq.py $1 -t -m final_model/final -o $2
+python model_seq2seq.py $1 -t -p -m final_model/final -o $3
