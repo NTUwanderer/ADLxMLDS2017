7f811114e4817cf46b5aeb6bef4ff592cb539320
NTUwanderer <harveyycyang@gmail.com>
2017-12-02 23:34:07 +0800

    First policy gradient

--- hw3/agent_dir/agent_pg.py
+++ hw3/agent_dir/agent_pg.py
@@ -1,4 +1,6 @@
 from agent_dir.agent import Agent
+from agent_dir.RL_brain import PolicyGradient
+import numpy as np
 
 class Agent_PG(Agent):
     def __init__(self, env, args):
@@ -17,7 +19,8 @@ class Agent_PG(Agent):
         # YOUR CODE HERE #
         ##################
 
-        env.seed(1)
+        self.env = env
+        self.env.seed(1)
 
 
     def init_game_setting(self):
@@ -30,7 +33,16 @@ class Agent_PG(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        pass
+        if not hasattr(self, 'RL'):
+        
+            self.RL = PolicyGradient(
+                n_actions=self.env.get_action_space().n,
+                n_features=self.env.get_observation_space().shape,
+                learning_rate=0.02,
+                reward_decay=0.99,
+            )
+
+            self.RL.restore('models/model_pg-0')
 
 
     def train(self):
@@ -40,8 +52,43 @@ class Agent_PG(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        pass
+        self.RL = PolicyGradient(
+            n_actions=self.env.get_action_space().n,
+            n_features=self.env.get_observation_space().shape,
+            learning_rate=0.02,
+            reward_decay=0.99,
+        )
 
+        for i_episode in range(3000):
+        
+            observation = np.reshape(self.env.reset(), [-1])
+        
+            while True:
+        
+                action = self.RL.choose_action(observation)
+        
+                observation_, reward, done, info = self.env.step(action)
+        
+                self.RL.store_transition(observation, action, reward)
+        
+                if done:
+                    ep_rs_sum = sum(self.RL.ep_rs)
+        
+                    if 'running_reward' not in globals():
+                        running_reward = ep_rs_sum
+                    else:
+                        running_reward = running_reward * 0.99 + ep_rs_sum * 0.01
+
+                    print("episode:", i_episode, "  reward:", int(running_reward))
+        
+                    vt = self.RL.learn()
+        
+                    break
+        
+                observation = np.reshape(observation_, [-1])
+
+            if (i_episode + 1) % 30 == 0:
+                self.RL.save('models/model_pg', int((i_episode + 1) / 30 - 1))
 
     def make_action(self, observation, test=True):
         """
