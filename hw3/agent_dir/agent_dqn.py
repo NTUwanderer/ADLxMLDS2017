96756ad436986e41e7228d2fb78a844e63c29e53
NTUwanderer <harveyycyang@gmail.com>
2017-12-14 09:50:01 +0800

    DQN beta

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -1,4 +1,44 @@
 from agent_dir.agent import Agent
+import random
+import tensorflow as tf
+
+from agent_dir.dqn.agent import Agent as myAgent
+from agent_dir.config import get_config
+
+flags = tf.app.flags
+
+# Model
+flags.DEFINE_string('model', 'm1', 'Type of model')
+flags.DEFINE_boolean('dueling', False, 'Whether to use dueling deep q-network')
+flags.DEFINE_boolean('double_q', False, 'Whether to use double q-learning')
+
+# Environment
+flags.DEFINE_string('env_name', 'Breakout-v0', 'The name of gym environment to use')
+flags.DEFINE_integer('action_repeat', 4, 'The number of action to be repeated')
+
+# Etc
+flags.DEFINE_boolean('use_gpu', False, 'Whether to use gpu or not')
+flags.DEFINE_string('gpu_fraction', '1/1', 'idx / # of gpu fraction e.g. 1/3, 2/3, 3/3')
+flags.DEFINE_boolean('display', False, 'Whether to do display the game screen or not')
+flags.DEFINE_boolean('is_train', True, 'Whether to do training or testing')
+flags.DEFINE_integer('random_seed', 123, 'Value of random seed')
+
+FLAGS = flags.FLAGS
+
+# Set random seed
+tf.set_random_seed(FLAGS.random_seed)
+random.seed(FLAGS.random_seed)
+
+if FLAGS.gpu_fraction == '':
+  raise ValueError("--gpu_fraction should be defined")
+
+def calc_gpu_fraction(fraction_string):
+  idx, num = fraction_string.split('/')
+  idx, num = float(idx), float(num)
+
+  fraction = 1 / (num - idx + 1)
+  print(" [*] GPU : %.4f" % fraction)
+  return fraction
 
 class Agent_DQN(Agent):
     def __init__(self, env, args):
@@ -9,6 +49,22 @@ class Agent_DQN(Agent):
 
         super(Agent_DQN,self).__init__(env)
 
+        gpu_options = tf.GPUOptions(
+            per_process_gpu_memory_fraction=calc_gpu_fraction(FLAGS.gpu_fraction))
+
+        self.sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
+        config = get_config(FLAGS) or FLAGS
+        self.env = env
+        
+        if not tf.test.is_gpu_available() and FLAGS.use_gpu:
+            raise Exception("use_gpu flag is true when no GPUs are available")
+
+        if not FLAGS.use_gpu:
+            config.cnn_format = 'NHWC'
+
+        self.agent = myAgent(config, env, self.sess)
+        print ('dqn shape: ', env.reset().shape)
+
         if args.test_dqn:
             #you can load your model here
             print('loading trained model')
@@ -16,6 +72,7 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
+        self.env = env
 
 
     def init_game_setting(self):
@@ -38,8 +95,7 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        pass
-
+        self.agent.train()
 
     def make_action(self, observation, test=True):
         """
